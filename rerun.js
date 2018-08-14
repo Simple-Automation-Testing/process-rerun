6593e554e7d8b4a499e657a7e76fffb5456746a2
potapovDim <potapovd3012@gmail.com>
2018-08-14 10:27:33 +0300

    share functionon from lib

--- rerun.js
+++ rerun.js
@@ -1,12 +1,9 @@
 const path = require('path')
 const fs = require('fs')
 const {exec} = require('child_process')
-
+const {walkSync, exeRun, runPromise, sleep, getRunCommand} = require('./lib')
 const argv = require('minimist')(process.argv.slice(2));
-const sleep = (time) => new Promise((res) => setTimeout(res, time))
-const failedByAssert = []
 
-const sleep = (time) => new Promise(res => setTimeout(res, time))
 
 let stdOutAnalize = (stack) => true
 let maxSession = argv.sessionsCount || 5
@@ -16,74 +13,7 @@ let grepKeyword = ''
 let currentSessionCount = 0
 let intervalPoll = 1000
 let rerunCycleCB = async () => true
-
-const walkSync = function(dir, filelist = []) {
-  const files = fs.readdirSync(dir)
-  files.forEach(function(file) {
-    if(fs.statSync(path.join(dir, file)).isDirectory()) {
-      filelist = walkSync(path.join(dir, file), filelist)
-    }
-    else {filelist.push(path.join(dir, file))}
-  })
-  return filelist
-}
-
 let specsDir = path.resolve(process.cwd(), './specs')
-let getRunCommand = (file) => `${path.resolve(process.cwd(), './node_modules/.bin/protractor')} ${configFilePath} --specs ${file}`
-const runPromise = (cmd) => new Promise((res) => {
-  const now = +Date.now(); const longestTest = 450000
-  const proc = exec(cmd)
-  let fullStack = ''
-  const watcher = setInterval(() => {if(+Date.now() - now > longestTest) {clearInterval(watcher); proc.kill(); res(cmd)} }, 15000)
-
-  proc.on('exit', () => {clearInterval(watcher)})
-  proc.stdout.on('data', (data) => {fullStack += data.toString()})
-  proc.on('error', (e) => {console.error(e)})
-
-  proc.on('close', (code) => {
-    if(code !== 0 && stdOutAnalize(fullStack)) {
-      res(cmd)
-    } else {
-      if(code !== 0) {failedByAssert.push(cmd)}; res(null)
-    }
-  })
-})
-
-async function exeRun(runArr, failArr = []) {
-  runArr = runArr || walkSync(specsDir).map(getRunCommand)
-
-  runArr = runArr.filter(function(cmd) {return cmd.includes(grepKeyword)})
-
-  const failedTests = await new Array(rerunCount).join('_').split('_').reduce((resolver) => {
-    return resolver.then(resolvedArr => performRun(resolvedArr, []).then(failedArr => failedArr))
-  }, Promise.resolve(runArr))
-
-  async function runCommandsArr(runnCommandsArr, failedArr) {
-    if(maxSession > currentSessionCount && runnCommandsArr.length) {
-      currentSessionCount += 1
-      const result = await runPromise(runnCommandsArr.splice(0, 1)[0]).catch(console.error)
-      if(result) {failedArr.push(result)}
-      currentSessionCount -= 1
-    }
-  }
-
-  async function performRun(runSuits, failedRun) {
-    const asserter = setInterval(() => runCommandsArr(runSuits, failedRun), intervalPoll)
-
-    do {
-      if(runSuits.length) {await runCommandsArr(runSuits, failedRun)}
-      if(currentSessionCount) {await sleep(2500)}
-    } while(runSuits.length || currentSessionCount)
-
-    await rerunCycleCB()
-    clearInterval(asserter)
-    return failedRun
-  }
-
-  console.log(failedTests.length, 'Failed test count')
-  return [...failedTests, ...failedByAssert]
-}
-
 
 module.exports = {
   getReruner: function({
@@ -102,6 +32,7 @@ module.exports = {
     intervalPoll = isNaN(Number(pollTime)) ? 1000 : Number(intervalPoll)
     return exeRun
   },
+
   getSpecCommands: function(pathToSpecDir, getRunCommandPattern) {
     return walkSync(pathToSpecDir).map(getRunCommandPattern)
   }
