af4756cdd87661a81176776870e4da709c05264d
Potapov Dmitriy <potapovd3012@gmail.com>
2018-08-18 13:42:12 +0300

    refactor to index

--- rerun.js
+++ rerun.js
@@ -1,36 +1,9 @@
-const path = require('path')
-const fs = require('fs')
-const {exec} = require('child_process')
-const {walkSync, exeRun, runPromise, sleep, getRunCommand} = require('./lib')
-const argv = require('minimist')(process.argv.slice(2));
 
-
-let stdOutAnalize = (stack) => true
-let maxSession = argv.sessionsCount || 5
-let rerunCount = argv.count || 2
-let configFilePath = argv.configPath || path.resolve(process.cwd(), './protractor.conf.js')
-let grepKeyword = ''
-let currentSessionCount = 0
-let intervalPoll = 1000
-let rerunCycleCB = async () => true
-let specsDir = path.resolve(process.cwd(), './specs')
+const {walkSync, buildExeRun} = require('./lib')
 
 module.exports = {
-  getReruner: function({
-    maxSessionCount = 5,
-    specRerunCount = 2,
-    stackAnalize = (stack) => true,
-    everyCycleCallback = async () => true,
-    grepWord = '',
-    pollTime = 1000}) {
-
-    maxSession = maxSessionCount
-    rerunCount = specRerunCount
-    stdOutAnalize = stackAnalize
-    grepKeyword = grepWord
-    rerunCycleCB = everyCycleCallback
-    intervalPoll = isNaN(Number(pollTime)) ? 1000 : Number(intervalPoll)
-    return exeRun
+  getReruner: function(optsObj) {
+    return buildExeRun(optsObj)
   },
 
   getSpecCommands: function(pathToSpecDir, getRunCommandPattern) {
