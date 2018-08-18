5fc45d9c8c69161df20f8682134c34f467463c1d
Potapov Dmitriy <potapovd3012@gmail.com>
2018-08-18 13:54:50 +0300

    small fixes

--- rerun.js
+++ rerun.js
@@ -5,7 +5,6 @@ module.exports = {
   getReruner: function(optsObj) {
     return buildExeRun(optsObj)
   },
-
   getSpecCommands: function(pathToSpecDir, getRunCommandPattern) {
     return walkSync(pathToSpecDir).map(getRunCommandPattern)
   }
