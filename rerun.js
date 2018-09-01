68fc4eeb30472beff28ecb1fe04273e3727b53df
Potapov Dmitriy <potapovd3012@gmail.com>
2018-09-01 10:02:35 +0300

    add flag

--- rerun.js
+++ rerun.js
@@ -5,6 +5,7 @@ module.exports = {
   getReruner: function(optsObj) {
     return buildExeRun(optsObj)
   },
+  getSpecFilesArr: walkSync,
   getSpecCommands: function(pathToSpecDir, getRunCommandPattern) {
     return walkSync(pathToSpecDir).map(getRunCommandPattern)
   }
