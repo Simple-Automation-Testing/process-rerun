9f9d8a0ccdc93386c2be37e028a637f3cd43e85a
potapovDim <potapovd3012@gmail.com>
2019-03-07 18:20:44 +0200

    add test

---
+++ unit_specs/helper.spec.js
@@ -0,0 +1,14 @@
+const {getPollTime, getFormedRunCommand} = require('../lib/helpers')
+const {expect} = require('chai')
+const path = require('path')
+
+describe('helper functions ', () => {
+  it('getPollTime', () => {
+    expect(getPollTime('das')).to.eq(1000)
+    expect(getPollTime(null)).to.eq(1000)
+    expect(getPollTime({})).to.eq(1000)
+    expect(getPollTime()).to.eq(1000)
+    expect(getPollTime(1)).to.eq(1)
+    expect(getPollTime([])).to.eq(1000)
+  })
+})
