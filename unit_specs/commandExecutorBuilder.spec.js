17dea841807c7c749acf07f0eaabcb9d9050f252
potapovDim <potapovd3012@gmail.com>
2019-03-09 12:11:08 +0200

    add test for current variable

--- unit_specs/commandExecutorBuilder.spec.js
+++ unit_specs/commandExecutorBuilder.spec.js
@@ -62,4 +62,81 @@ describe('buildCommandExecutor', () => {
     expect(holder.cmd).to.eq(cmd)
     expect(holder.stack).to.eq('test\n')
   })
+
+  it('longestProcessTime', async () => {
+    {
+      const failedByAssert = []
+      let holder = null
+      const stackAnalize = (stack) => {
+        holder = {}
+        holder.fromStackAnalize = stack
+        return false
+      }
+      const cmd = `node -e "(async function() {await new Promise((res) => setTimeout(() => {
+        res(process.exit(1))
+      }, 25000))})()"`
+
+      const executeCommandAsync = buildCommandExecutor(failedByAssert, {stackAnalize, longestProcessTime: 1})
+      await executeCommandAsync(cmd)
+      expect(holder).to.eq(null)
+    }
+    {
+      const failedByAssert = []
+      let holder = null
+      const stackAnalize = (stack) => {
+        holder = {}
+        holder.fromStackAnalize = stack
+        return false
+      }
+      const cmd = `node -e "(async function() {await new Promise((res) => setTimeout(() => {
+        res(process.exit(1))
+      }, 1))})()"`
+
+      const executeCommandAsync = buildCommandExecutor(failedByAssert, {stackAnalize, longestProcessTime: 10000})
+      await executeCommandAsync(cmd)
+      expect(holder).to.not.eq(null)
+    }
+  })
+
+  it('currentExecutionVariable', async () => {
+    const failedByAssert = []
+    let holder = null
+    const cmd = `node -e "console.log('test'); process.exit(1)"`
+
+    const stackAnalize = (stack) => {
+      holder = {}
+      holder.fromStackAnalize = stack
+      return false
+    }
+
+    const addCommandOptionsAfterRun = (cmd, stack) => {
+      if(!holder) holder = {}
+      holder.cmd = cmd
+      holder.stack = stack
+    }
+    const currentExecutionVariable = 'CURRENT_COUNT'
+
+    const executeCommandAsync = buildCommandExecutor(failedByAssert, {
+      stackAnalize,
+      addCommandOptionsAfterRun,
+      currentExecutionVariable
+    })
+
+    const expetedReformatedCommandFirstIteration = `${currentExecutionVariable}=${1} ${cmd}`
+    const expetedReformatedCommandSecondIteration = `${currentExecutionVariable}=${2} ${cmd}`
+
+    // first iteration
+    await executeCommandAsync(cmd, 1)
+
+    expect(holder.fromStackAnalize).to.eq('test\n')
+    expect(holder.cmd).to.eq(expetedReformatedCommandFirstIteration)
+    expect(holder.stack).to.eq('test\n')
+
+    await executeCommandAsync(expetedReformatedCommandFirstIteration, 2)
+
+    expect(holder.fromStackAnalize).to.eq('test\n')
+    expect(holder.cmd).to.eq(expetedReformatedCommandSecondIteration)
+    expect(holder.stack).to.eq('test\n')
+
+  })
 })
