23302feafa0de5d6d9ddf025a085c619c6b4f65f
potapovDim <potapovd3012@gmail.com>
2019-03-09 11:34:14 +0200

    add tests

--- unit_specs/commandExecutorBuilder.spec.js
+++ unit_specs/commandExecutorBuilder.spec.js
@@ -36,5 +36,30 @@ describe('buildCommandExecutor', () => {
     const executeCommandAsync = buildCommandExecutor(failedByAssert, {addCommandOptionsAfterRun})
     await executeCommandAsync(cmd)
     expect(holder.cmd).to.eq(cmd)
+    expect(holder.stack).to.eql('test\n')
+  })
+
+  it('stackAnalize', async () => {
+    const failedByAssert = []
+    let holder = null
+    const cmd = `node -e "console.log('test'); process.exit(1)"`
+
+    const addCommandOptionsAfterRun = (cmd, stack) => {
+      if(!holder) holder = {}
+      holder.cmd = cmd
+      holder.stack = stack
+    }
+    const stackAnalize = (stack) => {
+      holder = {}
+      holder.fromStackAnalize = stack
+      return false
+    }
+    const executeCommandAsync = buildCommandExecutor(failedByAssert, {
+      addCommandOptionsAfterRun, stackAnalize
+    })
+    await executeCommandAsync(cmd)
+    expect(holder.fromStackAnalize).to.eq('test\n')
+    expect(holder.cmd).to.eq(cmd)
+    expect(holder.stack).to.eq('test\n')
   })
 })
