88a9a1d9e772bfd811d737b626533d6c9f2f47de
potapovDim <potapovd3012@gmail.com>
2019-09-02 16:46:29 +0300

    fix

--- lol.js
+++ lol.js
@@ -1,9 +1,1 @@
-const {spawn} = require('child_process')
-
-const cmd = ['node', ['-e', "console.log(\'test\')"]]
-
-const proc = spawn(...cmd)
-
-proc.stdout.on('data', (data) => {
-  console.log(data.toString('utf8'))
-})
+console.info('!!!!')
